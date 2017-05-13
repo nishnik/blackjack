92d65b7d02df8184b196fd8bda60747dff59d494
arunpatro <arun.kumar.patro@gmail.com>
2017-05-13 22:29:57 +0530

    clean getNewGrades.py

--- getNewGrades.py
+++ getNewGrades.py
@@ -1,6 +1,7 @@
 from subprocess import check_output
 import re
 import json
+import progressbar
 
 def load(file):
     def convert(input):
@@ -20,14 +21,16 @@ def save(dic, name):
     with open(name + ".json", "w") as outfile:
         json.dump(dic, outfile)
         
-jsessionid = '072D3AA2F31C9329C4462B539DF76056.worker3'
+cookie = '43E3828C7782BC1EDD306A57DF7FA26F.worker3'
         
 def getGrades(code):
     b = check_output(
-        "curl 'https://erp.iitkgp.ernet.in/Acad/Pre_Registration/subject_grade_status.jsp?subno={}' -H 'DNT: 1' -H 'Accept-Encoding: gzip, deflate, sdch, br' -H 'Accept-Language: en-US,en;q=0.8' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Cookie: JSESSIONID={}' -H 'Connection: keep-alive' --compressed".format(code,jsessionid), shell=True)
+        "curl 'https://erp.iitkgp.ernet.in/Acad/Pre_Registration/subject_grade_status.jsp?subno={}' -H 'DNT: 1' -H 'Accept-Encoding: gzip, deflate, sdch, br' -H 'Accept-Language: en-US,en;q=0.8' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Cookie: JSESSIONID={}' -H 'Connection: keep-alive' --compressed".format(code,cookie), shell=True);
     nums = re.findall('([A-Z\s]+)\(No. of Student\) : ([0-9]+)', b)
-    grades = {str(d[0].strip()): int(d[1]) for d in nums}
-    return grades
+    if len(nums)!=0:
+        return {str(d[0].strip()): int(d[1]) for d in nums}
+    else:
+        return {}
     
 def uniformizeGradesJSON(stats):
     g = ['EX','A','B','C','D','P','F']
@@ -39,14 +42,18 @@ def uniformizeGradesJSON(stats):
 def main():
     newGrades = {}
     allcourses = load('courses.json').keys()
-    for i in allcourses:
-        while True:
-            try:
-                newGrades[i] = {'grades': uniformizeGradesJSON(getGrades(i))}
-                print i, newGrades[i]
-            except Exception as e:
-                print i, e
-            break
+    with progressbar.ProgressBar(max_value=len(allcourses)) as bar:
+        for n, i in enumerate(allcourses):
+            while True:
+                try:
+                    g = getGrades(i);
+                    if len(g)!=0:
+                        newGrades[i] = {'grades': uniformizeGradesJSON(g)}
+                        print i, newGrades[i]
+                except Exception as e:
+                    print i, e
+                break
+            bar.update(n)
 
     save(newGrades, 'newGrades')
 
